8a9858ba29aabd8c2c96c8a991d03a815fca0b47
crown <1126713183@qq.com>
2017-12-23 22:01:34 +0800

    git commit -m 'update'

--- python3字典/dict.py
+++ python3字典/dict.py
@@ -1,4 +1,6 @@
-# coding:utf-8
+#usr/bin/python3
+#coding:utf-8
+#coding= gpk2312
 '''
 字典是另一种可变容器模型，且可存储任意类型对象。
 
@@ -27,5 +29,15 @@ dict5 ={'Name':'Runoob','Age':7,'class':'First'}
 print("更新字典中的信息")
 dict5['Age'] = 29
 dict5['School'] = '岸边中学'
-for temp in dict5:
-    print(temp)
+del dict5['Name']
+print(dict5)
+dict5.clear()
+type(dict5)
+dict6 = {'name': 'zhangbo','Age':56,'Name':'小菜鸟'}
+print("dict['Name']:",dict6['name'])
+print(len(dict6))
+print("字典内置函数")
+String =str(dict6)
+print(String)
+dict6.values()
+dict6.items()
